8ef257b4f8b11ac3c2f63d60267fbc3b53ca5e97
Lior Hasson <liorhasson@gmail.com>
2020-06-23 15:06:58 +0300

    few design updates and added alert when no characters in input field

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-function addItem2() {
-  var resElement = document.getElementById("body");
-  var theDiv = document.createElement("div");
-  var textURL = document.getElementById("theURL").value;
-  var textURL1 = document.createTextNode(textURL);
-  theDiv.appendChild(textURL1);
-  resElement.appendChild(theDiv);
-  if (document.getElementById("checkUrl").length === 0) {
-    document.getElementById("checkButton").disabled = true;
-  } else {
-    document.getElementById("checkButton").disabled = false;
-  }
-}
-
-function addItem() {
-  document.getElementById("list").innerHTML += "<li>Tomato</li>";
-}
-
-function success() {
-  if (document.getElementById("checkUrl").length === 0) {
-    document.getElementById("checkButton").disabled = true;
-  } else {
-    document.getElementById("checkButton").disabled = false;
-  }
-}
+// function stoppedTyping() {
+//   if (document.getElementById("checkUrl").length > 0) {
+//     document.getElementById("checkButton").disabled = false;
+//   } else {
+//     document.getElementById("checkButton").disabled = true;
+//   }
+// }
 
 function addItem3() {
-  
-  var theurl = document.getElementById('checkUrl').value;
-  alert(theurl);
+  var theurl = document.getElementById("checkUrl").value;
 
-  // check if not empty
-  //check format
-  checkurl(theurl);
+  if (theurl.length === 0) {
+    alert("הזן כתובת אתר אינטרנט לבדיקת נגישות");
+  } else {
+    checkurl(theurl);
+  }
 }
