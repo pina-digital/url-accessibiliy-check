447ec3adb7b98755b4d1215d3e871cb4b57847df
pina-digital <66746949+pina-digital@users.noreply.github.com>
2020-06-23 11:51:50 +0300

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    var result = $('#result');
+    var theURL = "https://pina.co.il/";
+    $.ajax({
+        type: 'GET',
+        //url: 'https://wave.webaim.org/api/request?key=Gf4NdsFF1629&url=' + theURL,
+        success: function(data) {
+            $('.loader-div').fadeOut();
+           
+                result.append('' +
+                    '<div class="postContainer">' +
+                    '<p> Success:' + data.status.success + '</p>' +
+                    '<h2> Page Title: ' + data.statistics.pagetitle + '</h2>' +
+                    '<h3> Page URL: ' + data.statistics.pageurl + '</h3>' +
+                    '<p> Errors: ' + data.categories.error.count + '</p>' +
+                    '<p> Contrast Errors: ' + data.categories.contrast.count + '</p>' +
+                    '<p> Alerts: ' + data.categories.alert.count + '</p>' +
+                    '<p> Features: ' + data.categories.feature.count + '</p>' +
+                    '<p> Structure Elements: ' + data.categories.structure.count + '</p>' +
+                    '<p> aria: ' + data.categories.aria.count + '</p>' +
+                    '<br></br>' +
+                    '<p> Creditsremaining: ' + data.statistics.creditsremaining + '</p>' +
+                    '</div>');
+
+        },
+        // error: function() {
+        //     $('#result').text('Error!');
+        // }
+        error: function (request, status, error) {
+            console.log(request.responseText);
+        }
+    });
+});
+
+
---
+++ script.js
@@ -0,0 +1,25 @@
+function addItem2() {
+  var resElement = document.getElementById("body");
+  var theDiv = document.createElement("div");
+  var textURL = document.getElementById("theURL").value;
+  var textURL1 = document.createTextNode(textURL);
+  theDiv.appendChild(textURL1);
+  resElement.appendChild(theDiv);
+  if (document.getElementById("checkUrl").length === 0) {
+    document.getElementById("checkButton").disabled = true;
+  } else {
+    document.getElementById("checkButton").disabled = false;
+  }
+}
+
+function addItem() {
+  document.getElementById("list").innerHTML += "<li>Tomato</li>";
+}
+
+function success() {
+  if (document.getElementById("checkUrl").length === 0) {
+    document.getElementById("checkButton").disabled = true;
+  } else {
+    document.getElementById("checkButton").disabled = false;
+  }
+}
