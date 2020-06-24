2eff68c4ffc2f90cb16436eeb3432d2122c49033
Lior Hasson <liorhasson@gmail.com>
2020-06-24 10:29:41 +0300

    url error messages and regex

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-// function stoppedTyping() {
-//   if (document.getElementById("checkUrl").length > 0) {
-//     document.getElementById("checkButton").disabled = false;
-//   } else {
-//     document.getElementById("checkButton").disabled = true;
-//   }
-// }
-
-function addItem3() {
+function pressButton() {
   var theurl = document.getElementById("checkUrl").value;
+  var feedback = document.getElementById("url-feedback");
+  var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
   if (theurl.length === 0) {
-    // alert("הזן כתובת אתר אינטרנט לבדיקת נגישות");
-    document.getElementById("url-feedback").innerHTML =
-      "נא להזין כתובת אתר אינטרנט";
+    feedback.innerHTML = "נא להזין כתובת אתר אינטרנט";
+    feedback.classList.add("show-feedback");
   } else {
     var element = document.getElementById("theloader");
     element.classList.add("line-scale");
     //check format
     checkurl(theurl);
-    document.getElementById("url-feedback").innerHTML = "";
+    feedback.innerHTML = "";
+    feedback.classList.remove("show-feedback");
   }
+}
+
+function urlValidate() {
+  var url = document.getElementById("checkUrl").value;
+  var feedback = document.getElementById("url-feedback");
+
+  var pattern = /(http[s]?:\/\/)?[^\s(["<,>]\.[^\s[",><]/;
+  if (pattern.test(url)) {
+    feedback.innerHTML = "הכתובת שהוזנה הינה כתובת חוקית";
+    feedback.classList.add("show-feedback");
+    return true;
+  }
+  feedback.innerHTML = "הכתובת שהוזנה אינה כתובת חוקית";
+  feedback.classList.add("show-feedback");
+  return false;
 }
