f1b247a63263bd488f1bf9ec32051e3572a36716
Lior Hasson <liorhasson@gmail.com>
2020-06-25 13:57:57 +0300

    fixed the url validation regex

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function pressButton() {
   var theurl = document.getElementById("checkUrl").value;
   var feedback = document.getElementById("url-feedback");
-  // var pattern = /(http[s]?:\/\/)?[^\s(["<,>]\.[^\s[",><]/;
 
   if (theurl.length === 0) {
     feedback.innerHTML = "נא להזין כתובת אתר אינטרנט";
@@ -21,10 +20,9 @@ function pressButton() {
 }
 
 function urlValidate(theurl) {
-  // var url = document.getElementById("checkUrl").value;
   var feedback = document.getElementById("url-feedback");
+  var pattern = /^((http(s){0,1}\:\/\/){0,1}([a-z|A-Z|0-9|\.|\-|_]){4,255}(\:\d{1,5}){0,1}){0,1}((\/([a-z|A-Z|0-9|\.|\-|_]|\%[A-F|a-f|0-9]{2}){1,255}){1,255}\/{0,1}){0,1}(|\/{0,1}\?[a-z|A-Z|0-9|\.|\-|_]{1,255}\=([a-z|A-Z|0-9|\.|\-|_|\+|\:]|\%[A-F|a-f|0-9]{2}|\&[a-z|A-Z]{2,12}\;){0,255}){0,1}((\&[a-z|A-Z|0-9|\.|\-|_]{1,255}\=([a-z|A-Z|0-9|\.|\-|_|\+|\:]|\%[A-F|a-f|0-9]{2}|\&[a-z|A-Z]{2,12}\;){0,255}){0,255})(\/{0,1}|\#([a-z|A-Z|0-9|\.|\-|_|\+|\:]|\%[A-F|a-f|0-9]{2}|\&[a-z|A-Z]{2,12}\;){0,255})$/;
 
-  var pattern = /(http[s]?:\/\/)?[^\s(["<,>]\.[^\s[",><]/;
   if (pattern.test(theurl)) {
     feedback.innerHTML = "הכתובת שהוזנה הינה כתובת חוקית";
     feedback.classList.add("show-feedback");
