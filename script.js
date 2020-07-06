1fae20f954114b6ec3f47d442b83e82088e78226
Lior Hasson <liorhasson@gmail.com>
2020-07-06 15:46:35 +0300

    lior remark

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function pressButton() {
 function urlValidate(theurl) {
   var feedback = document.getElementById("url-feedback");
   var pattern = /^((http(s){0,1}\:\/\/){0,1}([a-z|A-Z|0-9|\.|\-|_]){4,255}(\:\d{1,5}){0,1}){0,1}((\/([a-z|A-Z|0-9|\.|\-|_]|\%[A-F|a-f|0-9]{2}){1,255}){1,255}\/{0,1}){0,1}(|\/{0,1}\?[a-z|A-Z|0-9|\.|\-|_]{1,255}\=([a-z|A-Z|0-9|\.|\-|_|\+|\:]|\%[A-F|a-f|0-9]{2}|\&[a-z|A-Z]{2,12}\;){0,255}){0,1}((\&[a-z|A-Z|0-9|\.|\-|_]{1,255}\=([a-z|A-Z|0-9|\.|\-|_|\+|\:]|\%[A-F|a-f|0-9]{2}|\&[a-z|A-Z]{2,12}\;){0,255}){0,255})(\/{0,1}|\#([a-z|A-Z|0-9|\.|\-|_|\+|\:]|\%[A-F|a-f|0-9]{2}|\&[a-z|A-Z]{2,12}\;){0,255})$/;
-
+  //lior is nervous
   if (pattern.test(theurl)) {
     feedback.innerHTML = "הכתובת שהוזנה הינה כתובת חוקית";
     feedback.classList.add("show-feedback");
